78c8c52336c7c63b2752c4d3f674c1e70c41af90
SanKxi <1798615837@qq.com>
2022-05-15 15:41:42 +0800

    配置

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -18,7 +18,7 @@ from .base import *
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY =  os.environ.get('SECRET_KEY', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
